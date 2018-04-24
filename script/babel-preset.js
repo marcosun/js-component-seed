f0ebf7dbec7ddfa77de7c2cd4f470e0b7e6bc2e9
marcosun <marcoinbristol@gmail.com>
2018-04-24 19:34:38 +0800

    [core] Export build project to build folder.

--- script/babel-preset.js
+++ script/babel-preset.js
@@ -1,10 +1,27 @@
-const BABEL_ENV = process.env.BABEL_ENV;
+const ENV = process.env.BABEL_ENV;
+let config;
 
-module.exports = {
-  presets: [
-    ['env', {
-      modules: BABEL_ENV === 'cjs' ? 'commonjs' : false
-    }],
-    'stage-0'
-  ]
-};
+if (ENV === 'es') {
+  config = {};
+} else {
+  config = {
+    presets: [
+      [
+        'env',
+        {
+          targets: {
+            ie: 11,
+            edge: 14,
+            firefox: 45,
+            chrome: 49,
+            safari: 10,
+            node: '6.11',
+          },
+          modules: ENV === 'modules' ? false : 'commonjs',
+        },
+      ],
+    ],
+  };
+}
+
+module.exports = config;
