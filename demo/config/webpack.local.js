322ea1312c71dc80dbb581d975cd01d56d4df578
marcosun <marcoinbristol@gmail.com>
2018-07-23 12:02:53 +0800

    [core] Create four webpack config files for four environments

---
+++ demo/config/webpack.local.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const path = require('./path');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'eval-source-map',
+
+  devServer: {
+    contentBase: path.appDist,
+    historyApiFallback: true,
+    hot: true,
+  },
+
+  mode: 'development',
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // 热替换插件
+  ],
+});
